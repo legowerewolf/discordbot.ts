a279119267d4b80cddb90fbead22bb5b781cc7e9
Duncan Gibson <legowerewolf@gmail.com>
2018-08-03 18:40:20 -0700

    TypeScriptify IP reporter.

--- src/intentHandlers/ip-finder.js
+++
@@ -1,7 +0,0 @@
-const fetch = require('node-fetch');
-
-module.exports = {
-    handler: function (eventData) {
-        fetch("http://ip-api.com/json").then((response) => response.json()).then((text) => { eventData.responseCallback(`your IP address is ${text.query}.`) });
-    }
-}
