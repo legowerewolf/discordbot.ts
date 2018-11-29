3cf0f446d4a651b7090403ae4c545183b5dc1c7e
Duncan Gibson <legowerewolf@gmail.com>
2018-11-29 14:36:13 -0800

    Abandon auto-update experiment.

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,30 @@
 import * as Fs from 'fs';
 import { DiscordBot } from './discordbot';
 import { ConfigElement } from './types';
-import { update } from './update';
 
-update().then((result) => {
+Fs.readFile("./config/defaults.json", function (err, defaultData) {
+    if (err) throw err;
 
-    Fs.readFile("./config/defaults.json", function (err, defaultData) {
-        if (err) throw err;
+    Fs.readFile("./config/config.json", function (err, customData) {
+        if (err) {
+            if (process.env.BotConfig) {
+                customData = Buffer.from(process.env.BotConfig);
+            } else { throw err; }
+        }
 
-        Fs.readFile("./config/config.json", function (err, customData) {
-            if (err) {
-                if (process.env.BotConfig) {
-                    customData = Buffer.from(process.env.BotConfig);
-                } else { throw err; }
-            }
+        let defaultConfig: ConfigElement = JSON.parse(defaultData.toString());
+        let customConfig = JSON.parse(customData.toString());
 
-            let defaultConfig: ConfigElement = JSON.parse(defaultData.toString());
-            let customConfig = JSON.parse(customData.toString());
+        if (!customConfig.instances) {
+            customConfig = { instances: [customConfig] };
+        }
 
-            if (!customConfig.instances) {
-                customConfig = { instances: [customConfig] };
-            }
-
-            customConfig.instances.forEach((element: ConfigElement) => {
-                let config = { ...defaultConfig, ...element };
-                config.intents = element.intents ? defaultConfig.intents.concat(element.intents) : defaultConfig.intents;
-                let bot = new DiscordBot(config);
-                bot.start();
-            });
-
+        customConfig.instances.forEach((element: ConfigElement) => {
+            let config = { ...defaultConfig, ...element };
+            config.intents = element.intents ? defaultConfig.intents.concat(element.intents) : defaultConfig.intents;
+            let bot = new DiscordBot(config);
+            bot.start();
         });
-    });
 
+    });
 });
