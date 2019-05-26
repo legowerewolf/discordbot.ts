919743b9f9bcb72efdb61eeefd3c3e568abfb0a1
Duncan Gibson <legowerewolf@gmail.com>
2019-05-26 14:07:02 -0700

    Formatting plus return a special value for unknown/unconfident values.

--- src/brain.ts
+++ src/brain.ts
@@ -1,33 +1,33 @@
 // Brain is based on https://github.com/andrew-templeton/bottie
 
-import { LogisticRegressionClassifier } from 'natural';
-import { NaturalGuess } from './types';
+import { LogisticRegressionClassifier } from "natural";
+import { NaturalGuess } from "./types";
 
 export class Brain {
-    classifier: LogisticRegressionClassifier;
-    minConfidence: number;
+	classifier: LogisticRegressionClassifier;
+	minConfidence: number;
 
-    constructor(confidence: number) {
-        this.classifier = new LogisticRegressionClassifier();
-        this.minConfidence = confidence;
-    }
+	constructor(confidence: number) {
+		this.classifier = new LogisticRegressionClassifier();
+		this.minConfidence = confidence;
+	}
 
-    teach(samples: Array<string>, label: string) {
-        samples.forEach((sample: string) => {
-            this.classifier.addDocument(sample.toLowerCase(), label);
-        })
-    }
+	teach(samples: Array<string>, label: string) {
+		samples.forEach((sample: string) => {
+			this.classifier.addDocument(sample.toLowerCase(), label);
+		});
+	}
 
-    train() {
-        this.classifier.train();
-    }
+	train() {
+		this.classifier.train();
+	}
 
-    interpret(sample: string) {
-        let guesses = this.classifier.getClassifications(sample.toLowerCase()) as any as Array<NaturalGuess>;
-        let guess = guesses.reduce((accum, newVal) => {
-            return accum && accum.value > newVal.value ? accum : newVal;
-        })
+	interpret(sample: string) {
+		let guesses = (this.classifier.getClassifications(sample.toLowerCase()) as any) as Array<NaturalGuess>;
+		let guess = guesses.reduce((accum, newVal) => {
+			return accum && accum.value > newVal.value ? accum : newVal;
+		});
 
-        return guess.value > this.minConfidence ? guess : null;
-    };
+		return guess.value > this.minConfidence ? guess : { label: "_unknown", value: 0 };
+	}
 }
