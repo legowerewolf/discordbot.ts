c0da6ebbfb1af743f651329bdd02853446b9b595
Duncan Gibson <legowerewolf@gmail.com>
2019-06-07 21:46:38 -0700

    Add plugins on array creation, rather than pushing afterwards.

--- src/discordbot.ts
+++ src/discordbot.ts
@@ -76,9 +76,7 @@ export class DiscordBot {
 			this.client.on(element.event, element.handler);
 		});
 
-		this.plugins = new Array<Plugin>();
-
-		this.plugins.push(...this.config.plugins.map((name: string) => require(/* webpackMode: "eager" */ `./plugins/${name}`).default).map((plugin: PluginClass) => new plugin()));
+		this.plugins = new Array<Plugin>(...this.config.plugins.map((name: string) => require(/* webpackMode: "eager" */ `./plugins/${name}`).default).map((plugin: PluginClass) => new plugin()));
 		this.plugins.forEach((p: Plugin) => p.inject(this)); // Inject all plugins
 		this.console(ErrorLevels.Info, `Loaded plugins: ${this.config.plugins.join(", ")}`);
 	}
