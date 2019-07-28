baf29e5438562bae48fae9dc026b6b57a7b28013
Duncan Gibson <legowerewolf@gmail.com>
2019-07-27 21:36:02 -0700

    Remove a bunch of old code.

--- src/plugins/presence-roles.ts
+++ src/plugins/presence-roles.ts
@@ -1,4 +1,4 @@
-import { GuildMember, Role } from "discord.js";
+import { Role } from "discord.js";
 import { ErrorLevels } from "legowerewolf-prefixer";
 import { DiscordBot } from "../discordbot";
 import { getPropertySafe } from "../helpers";
@@ -12,7 +12,7 @@ export default class PresenceRoles extends Plugin {
 			if (
 				getPropertySafe(oldMember, ["presence", "game", "name"]) == getPropertySafe(newMember, ["presence", "game", "name"]) || // they haven't changed games
 				oldMember.user.bot || // they're a bot
-				!oldMember.guild.me.hasPermission("MANAGE_ROLES") // I can't mess with the
+				!oldMember.guild.me.hasPermission("MANAGE_ROLES") // I can't mess with roles on this server
 			)
 				return;
 
@@ -38,84 +38,6 @@ export default class PresenceRoles extends Plugin {
 					.catch((reason) => context.console(ErrorLevels.Error, `Error adding role. ${reason}`));
 			}
 		});
-	}
-
-	inject_old(context: DiscordBot) {
-		context.client.on(
-			"presenceUpdate",
-
-			(oldMember: GuildMember, member: GuildMember) => {
-				let guild = oldMember.guild;
-				let gameRoles = guild.roles.filter((value: Role) => value.name.startsWith(role_prefix));
-				if (guild.me.permissions.has("MANAGE_ROLES") && oldMember.presence.game != member.presence.game && !member.user.bot) {
-					let instance = Math.random();
-
-					let currentGame = member.presence.game;
-					if (currentGame != null) {
-						new Promise((resolve, reject) => {
-							// Find or make the role
-							let r = gameRoles.find((value: Role) => value.name == `${role_prefix}${currentGame.name}`);
-							resolve(
-								r != null
-									? r
-									: guild.createRole({ name: `${role_prefix}${currentGame.name}`, mentionable: true }).then(
-											(role) => {
-												context.console(ErrorLevels.Info, `Created role: ${role.guild.name}/${role.name} (instance: ${instance})`);
-												return Promise.resolve(role);
-											},
-											(error) => {
-												context.console(ErrorLevels.Error, "Error on role creation.");
-												context.console(ErrorLevels.Error, `${error}`);
-												return Promise.resolve(error);
-											}
-									  )
-							);
-						}).then((role: Role) => {
-							// Assign the user to the role
-							member.addRole(role).then(
-								(user) => {
-									context.console(ErrorLevels.Info, `Added role to user: ${role.guild.name}/${role.name} to ${user.displayName} (instance: ${instance})`);
-								},
-								(error) => {
-									context.console(ErrorLevels.Error, `Error on role assignment. ${role.guild.name}/${role.name} (${role}, deleted: ${(role as any).deleted}, instance: ${instance})`);
-									context.console(ErrorLevels.Error, `${error}`);
-								}
-							);
-						});
-					}
-
-					//Clean up other roles
-					member.roles
-						.filter((value: Role) => value.name.startsWith(role_prefix) && (currentGame != null ? value.name != `${role_prefix}${currentGame.name}` : true))
-						.forEach((role) => {
-							member
-								.removeRole(role)
-								.then((member) => {
-									if (role.members.size == 0) {
-										role.delete().then(
-											() => {
-												context.console(ErrorLevels.Info, `Deleted role: ${role.guild.name}/${role.name} (instance: ${instance})`);
-											},
-											(error) => {
-												context.console(ErrorLevels.Error, `Error on role deletion. ${role.guild.name}/${role.name} (${role}, deleted: ${(role as any).deleted}, instance: ${instance})`);
-												context.console(ErrorLevels.Error, `${error}`);
-											}
-										);
-									}
-								})
-								.then(
-									() => {
-										context.console(ErrorLevels.Info, `Removed role from user: ${role.guild.name}/${role.name} from ${member.displayName} (instance: ${instance})`);
-									},
-									(error) => {
-										context.console(ErrorLevels.Error, `Error on role removal. ${role.guild.name}/${role.name} (${role}, deleted: ${(role as any).deleted}, instance: ${instance})`);
-										context.console(ErrorLevels.Error, `${error}`);
-									}
-								);
-						});
-				}
-			}
-		);
 	}
 
 	extract(context: DiscordBot) {
