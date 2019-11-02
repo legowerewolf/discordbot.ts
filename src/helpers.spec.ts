a78798c21025b2804f5a7a9808d5be2a3c7b6928
Duncan Gibson <legowerewolf@gmail.com>
2019-11-02 15:50:46 -0700

    Downgrade discord.js to latest stable release, remove bad test

--- src/helpers.spec.ts
+++ src/helpers.spec.ts
@@ -1,18 +1,8 @@
 import { expect } from "chai";
 import "mocha";
-import { getPropertySafe, valuesOf } from "./helpers";
+import { valuesOf } from "./helpers";
 
 describe("helpers", () => {
-	describe("getPropertySafe", () => {
-		const testObj = { a: { b: { c: true } } };
-		it("should return true", () => {
-			expect(getPropertySafe(testObj, ["a", "b", "c"])).to.equal(true);
-		});
-		it("should be undefined", () => {
-			expect(getPropertySafe(testObj, ["a", "c"]));
-		});
-	});
-
 	describe("valuesOf", () => {
 		it("should return [1,2,3]", () => {
 			expect(valuesOf({ one: 1, two: 2, three: 3 })).to.deep.equal([1, 2, 3]);
