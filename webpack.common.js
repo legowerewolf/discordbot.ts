5f9e943602f83a1c049028043c127c9663440955
Duncan Gibson <legowerewolf@gmail.com>
2019-11-04 12:02:13 -0800

    Fix module resolution order. Thanks @iCrawl !

--- webpack.common.js
+++ webpack.common.js
@@ -3,6 +3,7 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const { DefinePlugin } = require("webpack");
 const packagejson = require("./package.json");
 const childprocess = require("child_process");
+const webpack = require("webpack");
 
 module.exports = {
 	entry: {
@@ -26,6 +27,7 @@ module.exports = {
 		}),
 	],
 	resolve: {
+		mainFields: ["main", "module"],
 		extensions: [".ts", ".tsx", ".json", ".js"],
 	},
 	target: "node",
