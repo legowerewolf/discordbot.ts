78c998464245ed2de5a01e26b7afa3ff506092b6
Duncan Gibson <legowerewolf@gmail.com>
2019-11-30 22:07:23 -0800

    Initialize Sentry API on the shard manager

--- src/shard-manager.ts
+++ src/shard-manager.ts
@@ -1,9 +1,17 @@
+import { init } from "@sentry/node";
 import { ShardingManager } from "discord.js";
 import { defaultPrefixer, errorLevelPrefixer, ErrorLevels } from "legowerewolf-prefixer";
-import 'source-map-support/register';
+import "source-map-support/register";
 import { parseConfig } from "./helpers";
 
 parseConfig().then((config) => {
+	if (config.APIKeys.sentry)
+		init({
+			dsn: config.APIKeys.sentry,
+			// @ts-ignore - these values are filled in at build time.
+			release: `${META_VERSION} / ${META_HASH}`,
+		});
+
 	let manager = new ShardingManager(`./build/shard.js`, {
 		totalShards: "auto",
 		token: config.APIKeys.discord,
