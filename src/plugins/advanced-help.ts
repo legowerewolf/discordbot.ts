f5a1ad5ba604c2095d05d4ac5dc383d5e2dd4a92
Duncan Gibson <legowerewolf@gmail.com>
2019-12-09 15:31:01 -0800

    Update advanced-help.ts
    Formatting

--- src/plugins/advanced-help.ts
+++ src/plugins/advanced-help.ts
@@ -1,3 +1,4 @@
+import { stripIndents } from "common-tags";
 import { DiscordBot } from "../discordbot";
 import { randomElementFromArray, valuesOf } from "../helpers";
 import { CommunicationEvent, Plugin } from "../types";
@@ -6,16 +7,22 @@ export default class HelpPlugin extends Plugin {
 	inject(context: DiscordBot) {
 		context.handlers["help"] = (event: CommunicationEvent) => {
 			event.responseCallback(
-				`
-Here's what I can do for you on request:
+				stripIndents`
+                    Here's what I can do for you on request:
 
-${valuesOf(event.bot.config.intents)
-	.filter((intent) => intent.models && intent.description && intent.name)
-	.map((intent) => `**${intent.name}**\nInvocation example: \`${randomElementFromArray(intent.models)}\`\n${intent.description}`)
-	.join("\n\n")}
+                    ${valuesOf(event.bot.config.intents)
+						.filter((intent) => intent.models && intent.description && intent.name)
+						.map(
+							(intent) => stripIndents`
+                                **${intent.name}**
+                                Invocation example: \`${randomElementFromArray(intent.models)}\`
+                                ${intent.description}
+                            `
+						)
+						.join("\n\n")}
 
-*The above command models are picked at random from the many I respond to.*
-`
+                    *The above examples are picked at random from the many I respond to.*
+                    `
 			);
 		};
 	}
