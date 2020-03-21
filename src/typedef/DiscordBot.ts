a049f1a183f89ee4cebb0e705c583cce6d87528a
Duncan Gibson <legowerewolf@gmail.com>
2020-03-20 21:57:20 -0700

    Add plugin loading messages

--- src/typedef/DiscordBot.ts
+++ src/typedef/DiscordBot.ts
@@ -77,6 +77,7 @@ export class DiscordBot {
 		this.handlers = {};
 		this.plugins = new Array<Plugin<{}>>();
 		Object.keys(this.config.plugins).map(async (name: string) => {
+			this.console(`Loading plugin...`, { plugin: name });
 			import(`../plugins/${name}`).then(
 				(module: ModuleWithClassDefault<Plugin<{}>>) => {
 					const instance: Plugin<{}> = new module.default(this.config.plugins[name]);
