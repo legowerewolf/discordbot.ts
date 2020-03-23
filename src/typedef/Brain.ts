1a89bc0fdfd8e6f0d288808661c2a553007aa7cc
Duncan Gibson <legowerewolf@gmail.com>
2020-03-23 13:55:06 -0700

    Move the naturalguess definition

--- src/typedef/Brain.ts
+++ src/typedef/Brain.ts
@@ -1,29 +1,45 @@
 // Brain is based on https://github.com/andrew-templeton/bottie
 
 import { LogisticRegressionClassifier } from "natural";
-import { NaturalGuess } from "./NaturalGuess";
+
+export interface NaturalGuess {
+	label: string;
+	value: number;
+}
 
 export class Brain {
 	static readonly UncertainLabel = "_unknown";
 
-	classifier: LogisticRegressionClassifier;
+	private classifier: LogisticRegressionClassifier;
 	minConfidence: number;
 
-	constructor(confidence: number) {
+	constructor(minConfidence: number) {
 		this.classifier = new LogisticRegressionClassifier();
-		this.minConfidence = confidence;
+		this.minConfidence = minConfidence;
 	}
 
+	/**
+	 *
+	 * @param samples - sample data to prepare the [[Brain]] to recognize
+	 * @param label - a label for the given samples
+	 */
 	teach(samples: Array<string>, label: string): void {
 		samples.forEach((sample: string) => {
 			this.classifier.addDocument(sample.toLowerCase(), label);
 		});
 	}
 
+	/**
+	 * Train the classifier to recognize the samples provided
+	 */
 	train(): void {
 		this.classifier.train();
 	}
 
+	/**
+	 *
+	 * @param sample - a sample you want the brain to classify
+	 */
 	interpret(sample: string): NaturalGuess {
 		const guesses = this.classifier.getClassifications(sample.toLowerCase()) as Array<NaturalGuess>;
 		const guess = guesses.reduce((accum, newVal) => {
