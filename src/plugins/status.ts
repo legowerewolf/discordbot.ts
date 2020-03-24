9b48a7382c0493b6b5fa4946f17fc713e26663a4
Duncan Gibson <legowerewolf@gmail.com>
2020-03-24 15:33:48 -0700

    Add port config option

--- src/plugins/status.ts
+++ src/plugins/status.ts
@@ -2,9 +2,17 @@ import { createServer, Server } from "http";
 import { DiscordBot } from "../typedef/DiscordBot";
 import { Plugin } from "../typedef/Plugin";
 
-export default class StatusPlugin extends Plugin<{}> {
+interface Config {
+	port: number;
+}
+
+export default class StatusPlugin extends Plugin<Config> {
 	private context: DiscordBot;
 	private server: Server;
+
+	static defaultConfig = {
+		port: 8080,
+	};
 
 	inject(context: DiscordBot): void {
 		if (context.client.shard?.ids.indexOf(0) == -1) return;
@@ -14,14 +22,13 @@ export default class StatusPlugin extends Plugin<{}> {
 		this.server = createServer(async (req, res) => {
 			res.statusCode = 200;
 			res.setHeader("content-type", "application/json");
-
 			res.end(
 				JSON.stringify({
-					username: this.context.client.user.tag,
-					guildcount: await this.context.client.shard.broadcastEval("this.guilds.cache.size").then((results) => results.reduce((accum, cur) => accum + cur, 0)),
+					username: this.context.client.user?.tag,
+					guildcount: await this.context.client.shard.broadcastEval(`this.guilds.cache.size`).then((results) => results.reduce((accum, cur) => accum + cur, 0)),
 				})
 			);
-		}).listen(8080);
+		}).listen(this.config.port);
 	}
 
 	extract(): void {
