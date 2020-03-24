eee35e1f7ba4ec41de675df2c5355e31f3271068
Duncan Gibson <legowerewolf@gmail.com>
2020-03-24 12:38:39 -0700

    Implement userdata read/write

--- src/plugins/persist.ts
+++ src/plugins/persist.ts
@@ -1,4 +1,7 @@
 import { Firestore, Timestamp } from "@google-cloud/firestore";
+import { promiseRetry } from "../helpers/promiseRetry";
+import { queryWithObject } from "../helpers/queryWithObject";
+import { DiscordBot } from "../typedef/DiscordBot";
 import { Plugin } from "../typedef/Plugin";
 
 interface Config {
@@ -10,21 +13,46 @@ export default class Persist extends Plugin<Config> {
 		keyPath: "",
 	};
 
-	db: Firestore;
+	private db: Firestore;
+	private context: DiscordBot;
 
 	constructor(_config?: Config) {
 		super(_config);
 		this.db = new Firestore({ keyFilename: this.config.keyPath });
 	}
 
-	inject(): void {
+	inject(context: DiscordBot): void {
 		this.db
 			.collection("stats")
 			.doc("bot")
 			.set({ bootTime: Timestamp.fromDate(new Date()) }, { merge: true });
+
+		this.context = context;
 	}
 
 	extract(): void {
 		this.db.terminate();
+	}
+
+	async readUser<T>(userID: string, query: T): Promise<T> {
+		const userdoc = await this.db
+			.collection("users")
+			.doc(userID)
+			.get()
+			.then((docref) => docref.data());
+
+		return userdoc != undefined ? queryWithObject(userdoc, query) : undefined;
+	}
+
+	async writeUser(userID: string, data: object): Promise<FirebaseFirestore.WriteResult> {
+		return promiseRetry(
+			() => {
+				return this.db
+					.collection("users")
+					.doc(userID)
+					.set(data, { merge: true });
+			},
+			{ description: `firebase write on user ${userID}`, console: this.context.console }
+		);
 	}
 }
